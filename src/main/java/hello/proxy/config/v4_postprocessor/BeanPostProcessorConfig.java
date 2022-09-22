14246b5d8691fb53a3b1662eddfb8c7d6b8668d6
zidol <ajh3166@naver.com>
2022-09-22 23:52:24 +0900

    BeanPostProcessorConfig 주석 설명 추가

--- src/main/java/hello/proxy/config/v4_postprocessor/BeanPostProcessorConfig.java
+++ src/main/java/hello/proxy/config/v4_postprocessor/BeanPostProcessorConfig.java
@@ -13,6 +13,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+/**
+ * - '@Import({AppV1Config.class, AppV2Config.class})' : V3는 컴포넌트 스캔으로 자동으로 스프링 빈으로 등록되지만, V1, V2 애플리케이션은 수동으로 스프링 빈으로 등록해야
+ * 동작한다. ProxyApplication 에서 등록해도 되지만 편의상 여기에 등록하자.<br>
+ * - '@Bean' logTraceProxyPostProcessor() : 특정 패키지를 기준으로 프록시를 생성하는 빈 후처리기를 스프링 빈으로 등록한다. 빈 후처리기는 스프링 빈으로만 등록하면 자동으로 동작한다. 여기에 프록시를 적용할 패키지 정보( hello.proxy.app )와 어드바이저( getAdvisor(logTrace) )를 넘겨준다.<br>
+ * - 프록시를 생성하는 코드가 설정 파일에는 필요 없다. 순수한 빈 등록만 고민하면 된다. 프록시를 생성하고 프록시를 스프링 빈으로 등록하는 것은 빈 후처리기가 모두 처리해준다.
+ */
 @Slf4j
 @Configuration
 @Import({AppV1Config.class, AppV2Config.class})
