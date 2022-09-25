3858ad08ed21bf3237601c7879860955095c048c
zidol <ajh3166@naver.com>
2022-09-25 17:37:59 +0900

    @Aspect AOP 프록시 적용 예제

---
+++ src/main/java/hello/proxy/config/v6_app/aspect/LogTraceAspect.java
@@ -0,0 +1,39 @@
+package hello.proxy.config.v6_app.aspect;
+
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+import java.lang.reflect.Method;
+
+@Slf4j
+@Aspect//ㅇㅓ드바이저로 변환하여 저장(어드바이저 빌더)
+public class LogTraceAspect {
+    //어드바이스 로직
+    private final LogTrace logTrace;
+
+    public LogTraceAspect(LogTrace logTrace) {
+        this.logTrace = logTrace;
+    }
+
+    @Around("execution(* hello.proxy.app..*(..))")  //포인트컷
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        TraceStatus status = null;
+        try {
+            String message = joinPoint.getSignature().toShortString();
+            status = logTrace.begin(message);
+
+            //로직 호출
+            Object result = joinPoint.proceed();
+
+            logTrace.end(status);
+            return result;
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+}
